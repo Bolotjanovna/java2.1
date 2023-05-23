5edeee5d2eee683b063bcb15fe21908a8194ceb6
Bolotjanovna <toksonbaeva2104@gmail.com>
2023-05-23 18:22:19 +0500

    java

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        int ticket = 15000;
+        int mile = 20;
+        System.out.println((ticket / mile) + "бонус");
+    }
+}
